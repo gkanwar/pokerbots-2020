b5bf2c8498755988d849821da43a696e4b302bf9
Gurtej Kanwar <gurtej@mit.edu>
2020-01-28 22:56:11 -0500

    Fix run_tournament

--- util/run_tournament.py
+++ util/run_tournament.py
@@ -4,16 +4,18 @@ import sys
 
 sys.path.append(os.getcwd())
 import config as cfg
+import matplotlib.pyplot as plt
 import engine
 
 topdir = os.getcwd()
 
 def run_match(ind, bot1, bot2):
-    cfg.PLAYER_1_PATH = topdir + '/' + bot1
-    cfg.PLAYER_2_PATH = topdir + '/' + bot2
+    cfg.PLAYER_1_PATH = topdir + '/robots/' + bot1
+    cfg.PLAYER_2_PATH = topdir + '/robots/' + bot2
     os.mkdir(f'match{ind}')
     os.chdir(f'match{ind}')
     engine.Game().run()
+    plt.gca().set_prop_cycle(None)
     os.chdir('..')
 
 if __name__ == '__main__':
@@ -25,9 +27,11 @@ if __name__ == '__main__':
     os.makedirs(args.path, exist_ok=True)
     os.chdir(args.path)
     ind = 1
+    count = 10
     for i in range(len(bots)):
         for j in range(i+1, len(bots)):
-            print('Match {:d}: {} vs {}'.format(ind, bots[i], bots[j]))
-            run_match(ind, bots[i], bots[j])
-            ind += 1
+            for k in range(count):
+                print('Match {:d}: {} vs {}'.format(ind, bots[i], bots[j]))
+                run_match(ind, bots[i], bots[j])
+                ind += 1
     print(f'Tournament complete, outputs are in {args.path}')
